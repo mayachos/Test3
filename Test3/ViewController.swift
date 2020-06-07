d8ed4398e3e0efaadb5f229be2007ad98fbe1d29
maya <amacrk@i.softbank.jp>
2020-06-07 11:40:51 +0900

    Initial Commit

---
+++ Test3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test3
+//
+//  Created by maya on 2020/06/07.
+//  Copyright © 2020 maya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test3Tests/Test3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Test3Tests.swift
+//  Test3Tests
+//
+//  Created by maya on 2020/06/07.
+//  Copyright © 2020 maya. All rights reserved.
+//
+
+import XCTest
+@testable import Test3
+
+class Test3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
